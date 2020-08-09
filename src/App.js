3bf471c219bf2be73f8f131e96d387184672d704
Habib Afolabi <habibwritescode@gmail.com>
2020-08-09 09:45:41 +0100

    Component composition

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import React from 'react';
-import './App.css';
-import Sidebar from './components/sidebar/sidebar'
+import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <Sidebar />
-    </div>
-  );
+import Sidebar from './components/sidebar/sidebar'
+import Home from './pages/home/home-page'
+import BrowsePage from './pages/browse/browse-page'
+import SearchPage from './pages/search/search-page'
+
+import './App.css';
+import './App.sass'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Sidebar />
+        <Switch>
+          {/* <Route exact path='/' component={Home} /> */}
+          <Route exact path='/browse' component={BrowsePage} />
+          <Route exact path='/search' component={SearchPage} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
