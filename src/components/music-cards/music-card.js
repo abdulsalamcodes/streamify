e17d9930e4a03dce007e6f902cb19a17f8363034
Quadrizz <mackquadrizz@gmail.com>
2020-08-10 22:17:17 +0100

    working with API

---
+++ src/components/music-cards/music-card.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Musiclist from '../music-list/music-list'
+
+
+const MusicCard = ({info , info_1 , info_2})=>{
+    if (info){
+        return(
+            
+        )
+    }
+
+}
+
+export default MusicCard
--- src/components/music-list/music-list.js
+++ src/components/music-list/music-list.js
@@ -1,22 +1,22 @@
-import React, { Component} from 'react';
-import ArtistPic from './eminem-picture.jpg'
+import React from 'react';
+// import ArtistPic from './eminem-picture.jpg'
 import { ReactComponent as PlayListIcon } from './icons/playlist.svg'
 import './music-list.scss'
 
-function MusicList() {
+function MusicList(props) {
     return (
         <div className='wrapper'>
             <h1 className='heading'>Results</h1>
             <div className='container row'>
                 <div className='img-and-song row'>
-                    <img className='artist-pic' src={ArtistPic} />
+                    <img className='artist-pic' src={props.ArtistPic} alt = "Artist" />
                     <div className='song-info column'>
-                        <p className='song-title'>Song title</p>
-                        <span className='artist-name'>Artist name</span>
+                        <p className='song-title'>Song title : {props.title}</p>
+                        <span className='artist-name'>Artist : {props.artist}</span>
                     </div>
                 </div>
                 <div className='icon-and-duration row'>
-                    <span className='duration'>4:20</span>
+                    <span className='duration'>duration: {props.duration}</span>
                     <PlayListIcon className='playlist' />
                 </div>
             </div>
